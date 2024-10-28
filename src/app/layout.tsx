7d79e7f880cf52d1a591aa6ee3545632a08d78c8
onlineasd2 <mailonline95@gmail.com>
2024-10-28 15:48:29 +0500

    first_commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import "./styles.scss"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
